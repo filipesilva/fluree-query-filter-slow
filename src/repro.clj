fccf7753ecd9b1d7a61f8b749be23b16d4135ea1
Filipe Silva <filipematossilva@gmail.com>
2022-03-16 11:45:37 +0000

    use three-tuple filtering instead

--- src/repro.clj
+++ src/repro.clj
@@ -16,13 +16,25 @@
                  :_predicate/name :event/data
                  :_predicate/doc  "Event data serialized as an EDN string."
                  :_predicate/type :string
-                 :_predicate/fullText false}])
+                 :_predicate/fullText false}
+                {:_id               :_predicate
+                 :_predicate/name   :event/order
+                 ;; NOTE: the "strictly increasing" condition could be validated via specs:
+                 ;; - collection spec to ensure order is there
+                 ;; - predicate spec to ensure the new number is bigger than the max
+                 ;; This validation isn't happening here, we're just transacting "correct" data.
+                 :_predicate/doc    "Strictly increasing big int for event ordering."
+                 :_predicate/unique true
+                 :_predicate/type   :bigint}])
 
 
 (defn new-event [x]
   {:_id        :event
    :event/id   (str "uuid-" x)
-   :event/data "{}"})
+   :event/data "{}"
+   ;; Compute the new order number as 1 higher than last.
+   ;; NOTE: is max-pred-val efficient for very large collections? I don't know.
+   :event/order "#(inc (max-pred-val \"event/order\"))"})
 
 (defn spy
   ([x]
@@ -33,7 +45,6 @@
    x))
 
 (comment
-  ;; Create ledger
   (println "Create ledger")
   (def conn (fdb/connect "http://localhost:8090"))
   @(fdb/new-ledger conn ledger)
@@ -47,18 +58,17 @@
               vector
               (fdb/transact conn ledger)
               deref)
-        ;; Change this number to make more events
-        (take 1000 (range)))
-
+        ;; Change these number to make more events, or to create sub ranges.
+        (range 0 1000))
 
   (println "Query over all events")
   (time (-> conn
             (fdb/db ledger)
             (fdb/query {:select {"?event" ["*"]}
-                        :where  [["?event" "event/id", "?id"]]
+                        :where  [["?event" "event/id", "?order"]]
                         ;; Subject (?event here) is a monotonically incrementing bigint,
                         ;; so ordering by that gives us insertion order.
-                        :opts   {:orderBy ["ASC", "?event"]
+                        :opts   {:orderBy ["ASC", "event/order"]
                                  :limit 15000}})
             deref
             count))
@@ -66,24 +76,32 @@
   ;; Takes ~500ms for 5000 total events
 
   (println "Query over half the events")
-  (let [event-number-500-subject-id (-> conn
-                                        (fdb/db ledger)
-                                        (fdb/query {:select "?event"
-                                                    :where  [["?event" "event/id", "uuid-500"]]})
-                                        deref
-                                        first)]
-    (time (-> conn
+
+  ;; Find out the order number of a target id.
+  ;; NOTE: I tried doing this within the filtered query but wasn't able to unify
+  ;; ?order within the object-function.
+  (def target-id "uuid-500")
+  (def target-order (-> conn
+                        (fdb/db ledger)
+                        (fdb/query {:select "?order"
+                                    :where  [["?event" "event/id", target-id]
+                                             ["?event" "event/order" "?order"]]})
+                        deref
+                        first))
+
+  (time (-> conn
             (fdb/db ledger)
             (fdb/query {:select {"?event" ["*"]}
-                        :where  [["?event" "event/id", "?id"]
-                                 ;; Add a filter to only get half of the total events
-                                 {:filter [(str "(> ?event " event-number-500-subject-id ")")]}]
-                        :opts   {:orderBy ["ASC", "?event"]
+                        :where  [["?event" "event/order" (str "#(> ?o " target-order ")")]]
+                        :opts   {:orderBy ["ASC", "event/order"]
                                  :limit 15000}})
             deref
-            count)))
+            count))
   ;; Takes ~300ms for 1000 total events (500 returned)
-  ;; Takes ~2000ms for 5000 total events (2500 returned)
+  ;; Takes ~300ms for 5000 total events (2500 returned)
+  ;; Takes ~150ms for 5000 total events (1500 returned)
+  ;; Takes ~50ms for 5000 total events (500 returned)
+  ;; Takes ~2ms for 5000 total events (5 returned)
 
 
   (println "Delete ledger")
